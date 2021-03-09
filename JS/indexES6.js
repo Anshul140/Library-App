f1d8c057fb5ee158fd975338f09d5309f0dff91a
Anshul <anshulmahato61@gmail.com>
2021-03-09 20:11:11 +0530

    Initial Commit

---
+++ JS/indexES6.js
@@ -0,0 +1,98 @@
+console.log('This is ES6 version of project-2');
+
+class Book {
+    constructor(name, author, category) {
+        this.name = name;
+        this.author = author;
+        this.category = category;
+    }
+}
+
+class Display {
+    add(book) {
+        console.log('Adding to UI');
+        let tableBody = document.getElementById('tableBody');
+        let uiString = `
+                <tr>
+                    <td>${book.name}</td>
+                    <td>${book.author}</td>
+                    <td>${book.category}</td>
+                </tr>`;
+        tableBody.innerHTML += uiString;
+    }
+
+    clear() {
+        let libraryForm = document.getElementById('libraryForm');
+        libraryForm.reset(); //reset()- clears the inputs field after submission
+    }
+
+    validate(book) {
+        if (book.name.length < 2 || book.author.length < 2) {
+            return false;
+        }
+        else {
+            return true;
+        }
+    }
+
+    show(category, displayMessage){
+
+        let message = document.getElementById('message');
+        message.innerHTML = `<div class="alert alert-${category} alert-dismissible fade show" role="alert">
+                            <strong>Message:</strong> ${displayMessage}
+                            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                            <span aria-hidden="true">×</span>
+                            </button>
+                            </div>`;
+        
+        setTimeout(function () {
+            message.innerHTML = ''
+        }, 5000);
+    }
+
+}
+
+//Add submit event listener to libraryForm
+let libraryForm = document.getElementById('libraryForm');
+libraryForm.addEventListener('submit', libraryFormSubmit);
+
+function libraryFormSubmit(e){
+    //default behaviour of forms is to refresh the page as soon as submit button is clicked hence to prevent that bottom fn is written
+    
+    console.log('You have submitted library form');
+    let name = document.getElementById('bookName').value;
+    let author = document.getElementById('Author').value;
+    let category;
+
+    let fiction = document.getElementById('Fiction');
+    let programming = document.getElementById('Programming');
+    let romance = document.getElementById('Romance');
+
+    if(fiction.checked){
+        category = fiction.value;
+    }
+    else if(programming.checked){
+        category = programming.value;
+    }
+    else if(romance.checked){
+        category = romance.value;
+    }
+
+    let book = new Book(name, author, category);
+    console.log(book);
+
+    let display = new Display();
+    
+    if(display.validate(book)){
+    
+        display.add(book);
+        display.clear();
+        display.show('Success', 'Your book has been added successfully');
+    }
+    else{
+        display.show('Error', 'Sorry you cannot add this book');
+    }
+        
+
+    e.preventDefault();
+}
